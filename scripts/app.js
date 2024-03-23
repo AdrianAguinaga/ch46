862cbbdff8873a1e11938268f9ca6f18b371a891
nefersillo <adrian.aguinaga@uabc.edu.mx>
2024-03-23 10:28:23 -0700

    106.1

---
+++ scripts/app.js
@@ -0,0 +1,22 @@
+function sayHello(){
+    console.log("Hello");
+}
+
+function init() {
+  console.log("hello world!");
+  sayHello();
+    //retrive data
+    //hook events
+
+}
+
+for (let x=0; x<10; x++) {
+    x+1;
+}
+for (let x=0; x<10; x++) {
+    x+1;
+}
+
+
+window.onload = init;
+//try to specify the order of the arguments excecution
